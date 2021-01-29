1a4da1a243b92952792a6b54b1d6890566a2ed99
JGSims <josi3006@hotmail.com>
2021-01-29 15:55:11 -0500

    Built navbar component, working on routing

--- recipeezy/src/components/navComponent.js
+++ recipeezy/src/components/navComponent.js
@@ -1,14 +1,48 @@
-// import logo from './logo.svg';
 // import './App.css';
+import './components.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route, Link
+} from 'react-router-dom';
+import SearchPage from './searchComponent.js';
+
+
 
 function Navbar() {
-  return (
-    <div>
 
-        <h1>This is a navbar.</h1>
-        
-    </div>
-  );
+
+
+    return (
+
+        <div class="container">
+
+            <div class="navdiv">
+
+                
+                <Router>
+                <nav>
+                    <Link to="/"><i class="material-icons">home</i></Link>
+                    {/* <Link to="/dashboard">Dashboard</Link> */}
+                </nav>
+
+             
+                    <Route
+                        path="/"
+                        component={SearchPage}
+                        exact
+                    />
+                    {/* <Route
+                path="/dashboard"
+                component={DashboardComponent}
+            /> */}
+
+                </Router>
+            </div>
+
+        </div>
+    );
 }
 
 export default Navbar;
+
--- recipeezy/src/index.js
+++ recipeezy/src/index.js
@@ -12,12 +12,10 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <Navbar />
-    <HitlistPage />
+    <SearchPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
