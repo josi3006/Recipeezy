4f5aaa852b8ff2d968a2df39cccc0d95e8c61524
JGSims <josi3006@hotmail.com>
2021-02-05 10:20:01 -0500

    Added working search box to searchPage

--- recipeezy/src/components/searchComponent.js
+++ recipeezy/src/components/searchComponent.js
@@ -8,22 +8,51 @@ import React, { useEffect, useState } from "react";
 const SearchPage = () => {
 
     const [count, setCount] = useState(0);
+    const [searchTerms, setSearchTerms] = useState("");
 
-  
-    const handleSearch = (e) => {
+
+    const clickCount = (e) => {
         e.preventDefault();
         setCount(count + 1);
-        console.log(count + 'cliques');
+        console.log(count + ' cliques');
+    }
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        setSearchTerms(searchTerms);
+        console.log('You want to search: ' + searchTerms);
 
     }
-    
 
     return (
         <div>
 
             <Navbar />
 
-            <button onClick={handleSearch}>Click?</button>
+            <button onClick={clickCount}>Click?</button>
+
+            <form
+                className="m-5"
+                onSubmit={handleSearch}>
+
+                <div className='row'>
+                    <div className='col s12'>
+                        <div className='form-group'>
+                            <input
+                                className='form-control'
+                                placeholder='Search'
+                                onChange={(event) => setSearchTerms(event.currentTarget.value)}
+                                name='searchterms' 
+                                id='searchtermsbox'
+                                value={searchTerms}
+                            />
+                        </div>
+                    </div>
+                </div>
+
+<button type="submit">GO!</button>
+
+            </form>
 
             {/* <form
                 className='mt-5 py-5 px-5'
