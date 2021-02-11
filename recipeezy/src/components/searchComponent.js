b5c74dd348ace5995a7267aa2761477772e5c3ab
JGSims <josi3006@hotmail.com>
2021-02-10 20:56:41 -0500

    Added href to search hit list

--- recipeezy/src/components/searchComponent.js
+++ recipeezy/src/components/searchComponent.js
@@ -11,18 +11,6 @@ const SearchPage = () => {
 
     const [searchTerms, setSearchTerms] = useState("");
     const [hitList, setHitList] = useState([""]);
-    const [testProp, setTestProp] = useState("");
-
-
-    const sendIt = () => {
-        setTestProp('sup');
-    }
-
-
-    const handleSearch = (e) => {
-        e.preventDefault();
-        console.log('Here are the terms: ' + searchTerms);
-    };
 
 
     const searchOptions = {
@@ -42,10 +30,7 @@ const SearchPage = () => {
 
         axios.request(searchOptions)
             .then((response) => {
-                // console.log(response.data.results[0].title);
-                console.log('in axios ' + response.data.results[0].title);
-                console.log('in axios just data' + response.data);
-                console.log('as JSON ' + JSON.stringify(response.data.results))
+
                 setHitList(response.data.results);
             })
             .catch((error) => {
@@ -55,74 +40,39 @@ const SearchPage = () => {
     }
 
 
-
-    // const makeTheCall = () => {
-    //     axios.get(searchURL)
-    //         .then((response) => {
-    //             console.log(response);
-    //         });
-    // };
-
-
-
-    // useEffect(() => {
-    //     console.log('You want to search: ' + searchTerms)
-    // }, [searchTerms]);
-
-    // useEffect(() => {
-    //     console.log('in useEffect' + JSON.stringify(hitList.results[0]))
-    // },
-    //     [hitList]);
-
-    useEffect(() => {
-        console.log('console says ' + testProp);
-    }, [testProp]);
-
-
-
-
     return (
 
         <div>
 
+            <Navbar />
 
-            <StepsPage message="I made it!" />
+            <ul>
 
-            <button onClick={sendIt}>Send. It.</button>
+                {Object.keys(hitList).map((key) => {
+                    const hitItem = hitList[key];
 
-            <button onClick={makeTheCall}>DO an AJAX call now</button>
+                    return (
 
+                        <div key={hitItem.id}>
 
+                            <div>
 
+                                <div>
+                                    <li>{hitItem.title}</li>
+                                    <li><small><a href={hitItem.sourceUrl}>{hitItem.sourceUrl}</a></small></li>
+                                    
+                                </div>
 
+                            </div>
 
+                        </div>
 
+                    );
+                })}
 
-            
-			{Object.keys(hitList).map((key) => {
-				const hitItem = hitList[key];
+            </ul>
 
-				return (
-					<div key={hitItem.id}>
-						<div>
-							<div>
-								<h4>{hitItem.title}</h4><br />
-                                {hitItem.sourceUrl}	<br /><br />						
-
-							</div>
-						</div>
-					</div>
-				);
-			})}
-
-
-
-
-
-
-            <form
-                className="m-5"
-                onSubmit={handleSearch}>
+            <form className="m-5">
                 <div className='row d-flex justify-content-center'>
                     <div className='col s12 narrow'>
                         <div className='form-group'>
@@ -137,8 +87,11 @@ const SearchPage = () => {
                         </div>
                     </div>
                 </div>
-                <button type="submit">GO!</button>
+                {/* <button type="submit">GO!</button> */}
             </form>
+
+            <button onClick={makeTheCall}>Search</button>
+
 
             <div className='row' id="hitListRow">
                 <div className='col' id="hitListCol">
