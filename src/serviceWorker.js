4e549efbad0e0fe191a350767208fbe193899cde
JGSims <josi3006@hotmail.com>
2021-03-10 12:39:28 -0500

    Debugging serviceWorker

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -32,7 +32,7 @@ const isLocalhost = Boolean(
       }
   
       window.addEventListener('load', () => {
-        const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+        const swUrl = `${process.env.PUBLIC_URL}/serviceWorker.js`;
   
         if (isLocalhost) {
           // This is running on localhost. Let's check if a service worker still exists or not.
@@ -121,9 +121,9 @@ const isLocalhost = Boolean(
           registerValidSW(swUrl, config);
         }
       })
-      .catch(() => {
+      .catch((error) => {
         console.log(
-          'No internet connection found. App is running in offline mode.'
+          'No internet connection found. App is running in offline mode.' + error
         );
       });
   }
