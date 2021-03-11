d6c2ee996485f08dca7ecb900d8131f65c855c15
JGSims <josi3006@hotmail.com>
2021-03-11 14:33:34 -0500

    Trying to fix icon bug on mobile

--- src/components/navComponent.js
+++ src/components/navComponent.js
@@ -8,17 +8,17 @@ function Navbar(props) {
 
         <div className="container-fluid navdiv">
 
-            <i className="material-icons-outlined" onClick={props.resetEverythingButton}>&#xe88a;</i>
+            <i className="material-icons-outlined icons" onClick={props.resetEverythingButton}>&#xe88a;</i>
 
 
             { props.showHitListIcon ?
-                <i className="material-icons-outlined" onClick={props.reShowHitListButton}>&#xe8fa;</i> : null}
+                <i className="material-icons-outlined icons" onClick={props.reShowHitListButton}>&#xe8fa;</i> : null}
 
             { props.showIngredientListIcon ?
-                <i className="material-icons-outlined" onClick={props.showIngredientsButton}>&#xe547;</i> : null}
+                <i className="material-icons-outlined icons" onClick={props.showIngredientsButton}>&#xe547;</i> : null}
 
             { props.showRecipeStepsIcon ?
-                <i className="material-icons-outlined" onClick={props.showRecipeStepsButton}>&#xe242;</i> : null}
+                <i className="material-icons-outlined icons" onClick={props.showRecipeStepsButton}>&#xe242;</i> : null}
 
 
         </div>
