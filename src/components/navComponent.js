c326a8cfe03e3975fb94d580ce77ade367e90fae
JGSims <josi3006@hotmail.com>
2021-03-22 10:33:15 -0400

    Changing icon strategy to hosting icons

--- src/components/navComponent.js
+++ src/components/navComponent.js
@@ -1,21 +1,38 @@
 import './components.css';
 import React, { useState } from 'react';
+import { ReactComponent as HomeIconSVG } from '../images/home-black-48dp.svg';
+
+
+
 
 
 function Navbar(props) {
 
-    let iconClass;
-    props.showRecipeStepsIcon
-        ? (iconClass = "fi-xnluxl-home icons")
-        : (iconClass = "fi-xnluhl-shopping-cart icons")
 
+    const showMe = () => {
+        props.showRecipeStepsIcon ?
+            console.log('Yes Icon!') : console.log('No Is Icon.')
+    }
 
     return (
 
         <div className="container-fluid navdiv">
 
+            <HomeIconSVG
+                fill="currentColor"
+                // className="icons"
+                style={{color: "#4bccff"}}
 
-            <i className="fi-xnluxl-home icons" onClick={props.resetEverythingButton}></i>
+            />
+
+            <button onClick={showMe} />
+
+            { props.showRecipeStepsIcon ?
+                <p>Yes Is Icon!</p> :
+                <p>No Icon No.</p>}
+
+
+            {/* <i className="fi-xnluxl-home icons" onClick={props.resetEverythingButton}></i>
             <i className="fi-xnluxl-link icons" onClick={props.reShowHitListButton}></i>
             <i className="fi-xnluxl-ordered-list icons"></i>
 
@@ -24,12 +41,11 @@ function Navbar(props) {
 
 
             { props.showIngredientListIcon ?
-                (<i className="fi-xnluhl-shopping-cart icons" onClick={props.showIngredientsButton}></i>) : null}
+                (<i className="fi-xnluhl-shopping-cart icons" onClick={props.showIngredientsButton}></i>) : null} */}
 
             {/* { props.showRecipeStepsIcon ?
                 (<i className="fi-xnluxl-ordered-list icons" onClick={props.showRecipeStepsButton}></i>) : null} */}
 
-            <i className={iconClass}></i>
 
 
         </div>
